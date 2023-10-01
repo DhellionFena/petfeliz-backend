844677068828750b659fa3a19e77b110158b0d3a
Antônio César <antonioazevedo.dev@gmail.com>
2023-10-01 18:48:18 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,10 +11,20 @@ const main = () => {
 
     const corsOptions = {
         origin: '*',
-        credentials: true,
       };
 
     app.use(cors(corsOptions));
+
+    app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
+    app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+    });
 
     app.use('/pets', petRouter);
     app.use('/users', userRouter);
