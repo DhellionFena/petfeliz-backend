62388c2144395b60ca90b0358ef779357f51d818
Antônio César <antonioazevedo.dev@gmail.com>
2023-10-01 16:04:38 -0300

    adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { userRouter } = require('./routes/user.router');
 const { petRouter } = require('./routes/pet.router');
+const cors = require('cors');
 
 const main = () => {
     const app = express();
     const PORT = 8080;
 
     app.use(express.json());
+    app.use(cors());
 
     app.use('/pets', petRouter);
     app.use('/users', userRouter);
